ca8554dae001054073440447c3944ef0f40ca6e1
Celestin25 <celestinhakorimana25@gmail.com>
2023-03-28 23:06:22 +0200

    js

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="en" id="full">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="about.css">
-    <script src="aboutus.js"></script>
-</head>
-<body>
-    <img src="https://www.nutraingredients.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/nutraingredients.com/article/2020/02/24/study-different-foods-linked-to-different-types-of-stroke/10734771-1-eng-GB/Study-Different-foods-linked-to-different-types-of-stroke.jpg">
-    <div class="about-us">
-        <h2>About Us</h2>
-        <p>We are a group of students Addressing Cultural Shock.</p>
-        <p>Among African Tourists and Immigrants through a Recipe Directory App.</p>
-        <button class="button" id="learnMoreBtn">Learn More</button>
+var learnmoreButton = document.getElementById("learn-more-button");
+var learnmoreSection = document.getElementById("learn-more-section");
 
-      </div>  
-</body>
-</html>
+learnmoreButton.addEventListener("click", function() {
+  learnmoreSection.style.display = "block";
+  window.scrollTo(0,document.body.scrollHeight);
+});
+
