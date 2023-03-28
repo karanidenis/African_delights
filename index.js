57e11d39f7155274e747d29baf70f05a3d74deaf
Celestin25 <celestinhakorimana25@gmail.com>
2023-03-28 20:02:23 +0200

    task

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-// Add a hover effect to the Learn More button
-	const learnMoreBtn = document.querySelector('.button');
-	learnMoreBtn.addEventListener('mouseover', function() {
-		  learnMoreBtn.style.backgroundColor = '#3E8E41';
-	});
-learnMoreBtn.addEventListener('mouseout', function() {
-	  learnMoreBtn.style.backgroundColor = '#4CAF50';
-});
+<!DOCTYPE html>
+<html lang="en" id="full">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="about.css">
+    <script src="aboutus.js"></script>
+</head>
+<body>
+    <img src="https://www.nutraingredients.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/nutraingredients.com/article/2020/02/24/study-different-foods-linked-to-different-types-of-stroke/10734771-1-eng-GB/Study-Different-foods-linked-to-different-types-of-stroke.jpg">
+    <div class="about-us">
+        <h2>About Us</h2>
+        <p>We are a group of students Addressing Cultural Shock.</p>
+        <p>Among African Tourists and Immigrants through a Recipe Directory App.</p>
+        <button class="button" id="learnMoreBtn">Learn More</button>
 
+      </div>  
+</body>
+</html>
