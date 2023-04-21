3489c9becf1d493b2ea42492921a43af0721d46c
Praise-Atadja <p.atadja@alustudent.com>
2023-04-21 17:15:14 +0300

    update

--- webflask/static/homepage.js
+++ webflask/static/homepage.js
@@ -1,6 +1,6 @@
 // constants
 const body = document.querySelector("body"),
-    loader = document.querySelector(".loader-wrap"),
+    // loader = document.querySelector(".loader-wrap"),
     links = document.querySelectorAll('a[href="#"]'),
     nav = document.querySelector("header nav"),
     navToggle = document.querySelector("header nav .toggle"),
@@ -9,20 +9,57 @@ const body = document.querySelector("body"),
     navNavigationBarLi = document.querySelectorAll(
         "header nav .navigation-bar li"
     ),
-    headerText = document.querySelector("header .text"),
-    headerSection = document.querySelector("header"),
-    aboutSection = document.querySelector(".about-us"),
-    recipeSection = document.querySelector(".recipes"),
-    menuSection = document.querySelector(".menu"),
-    fixedImageSection = document.querySelector(".fixed-image"),
-    footerSection = document.querySelector("footer"),
-    dotOne = document.querySelector(".dots .one"),
-    dotTwo = document.querySelector(".dots .two"),
-    dotThree = document.querySelector(".dots .three"),
-    dots = document.querySelectorAll(".dots > div"),
-    logoImage = document.querySelector("header nav .logo img"),
-    svgDown = document.querySelector("header .arrow-down"),
-    svgUp = document.querySelector(".copyright .arrow-up"),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // headerText = document.querySelector("header .text"),
+    // headerSection = document.querySelector("header"),
+    // aboutSection = document.querySelector(".about-us"),
+    // recipeSection = document.querySelector(".recipes"),
+    // menuSection = document.querySelector(".menu"),
+    // fixedImageSection = document.querySelector(".fixed-image"),
+    // footerSection = document.querySelector("footer"),
+    // dotOne = document.querySelector(".dots .one"),
+    // dotTwo = document.querySelector(".dots .two"),
+    // dotThree = document.querySelector(".dots .three"),
+    // dots = document.querySelectorAll(".dots > div"),
+    // svgDown = document.querySelector("header .arrow-down"),
+    // svgUp = document.querySelector(".copyright .arrow-up"),
     menuImgs = document.querySelectorAll(".menu .menu-image-container img"),
     boxModel = document.querySelector(".menu .box-model"),
     menuImageContainer = document.querySelector(".menu-image-container"),
@@ -31,21 +68,21 @@ const body = document.querySelector("body"),
     pageTitle = document.querySelector("title");
 
 // remove loader
-function fadeOutEffect() {
-    const fadeEffect = setInterval(function() {
-        if (!loader.style.opacity) {
-            loader.style.opacity = 1;
-        }
-        if (loader.style.opacity > 0) {
-            loader.style.opacity -= 0.4;
-        } else {
-            body.classList.remove('stop-scroll');
-            loader.classList.add('remove');
-            clearInterval(fadeEffect);
-        }
-    }, 100);
-}
-window.addEventListener("load", fadeOutEffect);
+// function fadeOutEffect() {
+//     const fadeEffect = setInterval(function() {
+//         if (!loader.style.opacity) {
+//             loader.style.opacity = 1;
+//         }
+//         if (loader.style.opacity > 0) {
+//             loader.style.opacity -= 0.4;
+//         } else {
+//             body.classList.remove('stop-scroll');
+//             loader.classList.add('remove');
+//             clearInterval(fadeEffect);
+//         }
+//     }, 100);
+// }
+// window.addEventListener("load", fadeOutEffect);
 
 // prevent links click hash
 links.forEach(link =>
