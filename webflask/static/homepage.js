5589ee18c9de4dd5849865994fb46262468cb91b
Praise-Atadja <p.atadja@alustudent.com>
2023-04-26 15:05:41 +0300

    update homepage

--- webflask/static/homepage.js
+++ webflask/static/homepage.js
@@ -1,6 +1,9 @@
 // constants
+
+
+
 const body = document.querySelector("body"),
-    // loader = document.querySelector(".loader-wrap"),
+    loader = document.querySelector(".loader-wrap"),
     links = document.querySelectorAll('a[href="#"]'),
     nav = document.querySelector("header nav"),
     navToggle = document.querySelector("header nav .toggle"),
@@ -9,57 +12,20 @@ const body = document.querySelector("body"),
     navNavigationBarLi = document.querySelectorAll(
         "header nav .navigation-bar li"
     ),
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    // headerText = document.querySelector("header .text"),
-    // headerSection = document.querySelector("header"),
-    // aboutSection = document.querySelector(".about-us"),
-    // recipeSection = document.querySelector(".recipes"),
-    // menuSection = document.querySelector(".menu"),
-    // fixedImageSection = document.querySelector(".fixed-image"),
-    // footerSection = document.querySelector("footer"),
-    // dotOne = document.querySelector(".dots .one"),
-    // dotTwo = document.querySelector(".dots .two"),
-    // dotThree = document.querySelector(".dots .three"),
-    // dots = document.querySelectorAll(".dots > div"),
-    // svgDown = document.querySelector("header .arrow-down"),
-    // svgUp = document.querySelector(".copyright .arrow-up"),
+    headerText = document.querySelector("header .text"),
+    headerSection = document.querySelector("header"),
+    aboutSection = document.querySelector(".about-us"),
+    recipeSection = document.querySelector(".recipes"),
+    menuSection = document.querySelector(".menu"),
+    fixedImageSection = document.querySelector(".fixed-image"),
+    footerSection = document.querySelector("footer"),
+    dotOne = document.querySelector(".dots .one"),
+    dotTwo = document.querySelector(".dots .two"),
+    dotThree = document.querySelector(".dots .three"),
+    dots = document.querySelectorAll(".dots > div"),
+    logoImage = document.querySelector("header nav .logo img"),
+    svgDown = document.querySelector("header .arrow-down"),
+    svgUp = document.querySelector(".copyright .arrow-up"),
     menuImgs = document.querySelectorAll(".menu .menu-image-container img"),
     boxModel = document.querySelector(".menu .box-model"),
     menuImageContainer = document.querySelector(".menu-image-container"),
@@ -68,21 +34,21 @@ const body = document.querySelector("body"),
     pageTitle = document.querySelector("title");
 
 // remove loader
-// function fadeOutEffect() {
-//     const fadeEffect = setInterval(function() {
-//         if (!loader.style.opacity) {
-//             loader.style.opacity = 1;
-//         }
-//         if (loader.style.opacity > 0) {
-//             loader.style.opacity -= 0.4;
-//         } else {
-//             body.classList.remove('stop-scroll');
-//             loader.classList.add('remove');
-//             clearInterval(fadeEffect);
-//         }
-//     }, 100);
-// }
-// window.addEventListener("load", fadeOutEffect);
+function fadeOutEffect() {
+    const fadeEffect = setInterval(function() {
+        if (!loader.style.opacity) {
+            loader.style.opacity = 1;
+        }
+        if (loader.style.opacity > 0) {
+            loader.style.opacity -= 0.4;
+        } else {
+            body.classList.remove('stop-scroll');
+            loader.classList.add('remove');
+            clearInterval(fadeEffect);
+        }
+    }, 100);
+}
+window.addEventListener("load", fadeOutEffect);
 
 // prevent links click hash
 links.forEach(link =>
@@ -119,10 +85,10 @@ window.onscroll = function() {
     // make navbar fixed & change logo color
     if (window.pageYOffset > headerSection.offsetHeight - 75) {
         nav.classList.add("active");
-        logoImage.src = "https://res.cloudinary.com/abdel-rahman-ali/image/upload/v1535988525/logo-rosa.png";
+        logoImage.src = "../static/images/logo_image.jpg";
     } else {
         nav.classList.remove("active");
-        logoImage.src = "https://res.cloudinary.com/abdel-rahman-ali/image/upload/v1535988515/logo-rosa-white.png";
+        logoImage.src = "../static/images/logo_image.jpg";
     }
 
     // header welcome fade out and in
@@ -175,7 +141,7 @@ window.onscroll = function() {
 };
 
 // home page JS
-if (pageTitle.text === "AFRICAN DELIGHTS") {
+if (pageTitle.text === "ROSA- Restaurant") {
     // svg-down smooth scroll
     svgDown.addEventListener("click", () => {
         window.scroll({
